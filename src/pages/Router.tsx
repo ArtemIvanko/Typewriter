6c5e57ac5094cf9032248670b8851edb6a9492e9
ArtemIvanko <tema30k@gmail.com>
2024-10-02 09:53:15 +0300

    Created and used Home page

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter,  Navigate, Route, Routes } from "react-router-dom";
 import { Navigation } from "@shared/Navigation";
+import { Home } from "./Home";
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigation />}>
-        Hello
         <Route index element={<Navigate to="home" />} />
+        <Route path="home" element={<Home />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Route>
     </Routes>

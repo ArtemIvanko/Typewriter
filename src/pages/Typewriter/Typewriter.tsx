195b843a2cd19a542b58d095091d830ccfc98d06
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-01-01 10:05:25 +0200

    Fetched quotes using getQuotes function

--- src/pages/Typewriter/Typewriter.tsx
+++ src/pages/Typewriter/Typewriter.tsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+import { getQuotes } from "@/Api/getQuotes";
 import { TextField } from "@mui/material";
 
 export const Typewriter = () => {
+  const [quote, setQuote] = useState<string | null>(null);
+  
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const data = await getQuotes();
+        setQuote(data.content);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    
+    fetchQuote();
+  }, []);
+  
+  if (!quote) {
+    return <p>Loading...</p>;
+  }
+  
   return (
     <div>
       Typewriter

da965a652c3747b1e1721fa176088dad1777415c
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-01-01 10:11:18 +0200

    Created and used renderQuote function

--- src/pages/Typewriter/Typewriter.tsx
+++ src/pages/Typewriter/Typewriter.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import { getQuotes } from "@/Api/getQuotes";
 import { TextField } from "@mui/material";
+import styled from "@/DefaultTheme";
 
 export const Typewriter = () => {
   const [quote, setQuote] = useState<string | null>(null);
+  const [userInput, setUserInput] = useState("");
   
   useEffect(() => {
     const fetchQuote = async () => {
@@ -22,11 +24,28 @@ export const Typewriter = () => {
     return <p>Loading...</p>;
   }
   
+  const renderQuote = () => {
+    return quote?.split("").map((char: string, index: number) => {
+      const isCorrect = userInput[index] === char;
+      const isTyped = index < userInput.length;
+      
+      return (
+        <Letter key={index} $isCorrect={isCorrect} $isTyped={isTyped}>
+          {char}
+        </Letter>
+      );
+    });
+  };
+  
   return (
     <div>
       Typewriter
-      <p>quotes</p>
+      <p>{renderQuote()}</p>
       <TextField label="Type here" multiline fullWidth maxRows={8} variant="filled" />
     </div>
   );
 };
+
+const Letter = styled("span")<{ $isCorrect: boolean; $isTyped: boolean }>(({ $isCorrect, $isTyped, theme }) => ({
+  color: !$isTyped ? theme.palette.text.primary : $isCorrect ? theme.palette.success.main : theme.palette.error.main,
+}))

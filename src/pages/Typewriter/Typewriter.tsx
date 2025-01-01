bf4c1da204e3c19f38d2e538626c110af0c5b7b0
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-01-01 13:20:19 +0200

    Run prettier

--- src/pages/Typewriter/Typewriter.tsx
+++ src/pages/Typewriter/Typewriter.tsx
@@ -6,7 +6,7 @@ import styled from "@/DefaultTheme";
 export const Typewriter = () => {
   const [quote, setQuote] = useState<string | null>(null);
   const [userInput, setUserInput] = useState("");
-  
+
   useEffect(() => {
     const fetchQuote = async () => {
       try {
@@ -16,28 +16,30 @@ export const Typewriter = () => {
         console.error(err);
       }
     };
-    
+
     fetchQuote();
   }, []);
-  
-  const handleInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setUserInput(event.target.value);
-    
-    if (event.target.value.length === quote?.length) {
-      console.log("Quote completed");
-    }
-  }, [quote]);
-  
-  
+
+  const handleInputChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setUserInput(event.target.value);
+
+      if (event.target.value.length === quote?.length) {
+        console.log("Quote completed");
+      }
+    },
+    [quote],
+  );
+
   if (!quote) {
     return <p>Loading...</p>;
   }
-  
+
   const renderQuote = () => {
     return quote?.split("").map((char: string, index: number) => {
       const isCorrect = userInput[index] === char;
       const isTyped = index < userInput.length;
-      
+
       return (
         <Letter key={index} $isCorrect={isCorrect} $isTyped={isTyped}>
           {char}
@@ -45,16 +47,30 @@ export const Typewriter = () => {
       );
     });
   };
-  
+
   return (
     <div>
       Typewriter
       <p>{renderQuote()}</p>
-      <TextField label="Type here" multiline fullWidth maxRows={8} variant="filled" onChange={handleInputChange} />
+      <TextField
+        label="Type here"
+        multiline
+        fullWidth
+        maxRows={8}
+        variant="filled"
+        onChange={handleInputChange}
+      />
     </div>
   );
 };
 
-const Letter = styled("span")<{ $isCorrect: boolean; $isTyped: boolean }>(({ $isCorrect, $isTyped, theme }) => ({
-  color: !$isTyped ? theme.palette.text.primary : $isCorrect ? theme.palette.success.main : theme.palette.error.main,
-}))
+const Letter = styled("span")<{ $isCorrect: boolean; $isTyped: boolean }>(
+  ({ $isCorrect, $isTyped, theme }) => ({
+    color: !$isTyped
+      ? theme.palette.text.primary
+      : $isCorrect
+        ? theme.palette.secondary.bg
+        : theme.palette.warning.main,
+    fontSize: "1.5rem",
+  }),
+);
